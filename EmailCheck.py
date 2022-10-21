a8047b939a0c60c3fad9c00dccbd74fe3a1aaac9
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-10-21 09:42:22 +0530

    Create EmailCheck.py

---
+++ EmailCheck.py
@@ -0,0 +1,31 @@
+def fun(s):
+    try:
+        username, url = s.split('@')
+        website,extension = url.split('.')
+    except ValueError:
+        return False
+    
+    if username.replace('-','').replace('_','').isalnum() is False:
+        return False
+    
+    elif website.isalnum() is False:
+        return False
+    
+    elif len(extension) > 3:
+        return False
+    
+    else :
+        return True
+def sortmail(emails):
+    
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for i in range(n):
+        emails.append(input())
+
+se = sortmail(emails)
+se.sort()
+print(se)
